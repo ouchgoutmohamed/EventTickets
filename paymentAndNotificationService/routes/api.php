be2242cb83e198149b6f8363bc11efb509da4af6
AbdallahRdf <abdallahradfi@gmail.com>
2025-11-07 18:17:07 +0100

    Add enpoint to get all the payments of a single user

--- paymentAndNotificationService/routes/api.php
+++ paymentAndNotificationService/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PaymentController;
 
 Route::prefix('v1')->group(function () {
-    Route::post('payments', [PaymentController::class, 'store']);
+    Route::get('payments', [PaymentController::class, 'index']);
     Route::get('payments/{payment}', [PaymentController::class, 'show']);
+    
+    Route::post('payments', [PaymentController::class, 'store']);
 });
